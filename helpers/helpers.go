da22f5ee59313f54ec8ac97988574500929ddbc5
Vadim Inshakov <vadiminshakov@gmail.com>
2020-02-02 11:56:52 +0300

    file header added
    Signed-off-by: Author <vadiminshakov@gmail.com>
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2019 Vadim Inshakov
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package helpers
 
 import (
@@ -68,7 +84,9 @@ func Explore(wg *sync.WaitGroup, fab *models.Fabex) error {
 					//log.Printf("\nBlock finded\nBlock number: %d\nBlock hash: %s\nTx id: %s\nPayload:=%s\n", block.Blocknum, block.Hash, block.Txid, block.Payload)
 					fab.Db.Insert(block.Txid, block.Hash, block.Blocknum, block.Payload)
 				}
-			} else { break }
+			} else {
+				break
+			}
 		}
 	}
 	wg.Done()
