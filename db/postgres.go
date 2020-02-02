c87874bd38f8208e522a1649cf34d245a77ff720
Vadim Inshakov <vadiminshakov@gmail.com>
2020-02-02 11:56:52 +0300

    file header added
    Signed-off-by: Author <vadiminshakov@gmail.com>

--- db/postgres.go
+++ db/postgres.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2019 Vadim Inshakov
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package db
 
 import (
@@ -104,8 +120,8 @@ func (db *DB) Insert(txid, blockhash string, blocknum uint64, payload string) er
 
 func (db *DB) QueryBlockByHash(hash string) ([]Tx, error) {
 	//query := fmt.Sprintf(`
-    //    SELECT (txid, blockhash, blocknum, payload) FROM public.txs
-    //    WHERE blockhash='%s';`, hash)
+	//    SELECT (txid, blockhash, blocknum, payload) FROM public.txs
+	//    WHERE blockhash='%s';`, hash)
 	//
 	//var (
 	//	txid, blockhash string
