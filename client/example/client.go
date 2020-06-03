4c697a0697e4da5ee4e19f7d4c60cb8245846c35
Vadim Inshakov <vadiminshakov@gmail.com>
2020-06-03 14:10:32 +0300

    refactored API and fixes (#51)
    * ignore case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix example
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix test case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix REST API test
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * Fix CI (#47)
    
    * remove log msg
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * trigger CI on push && PR to 1.x, 2.x branches
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * new example request
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * handle nil and null block values
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add test case for querying all db entries
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * del Explore, add GetRange rpc
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * increment go version
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add handler for GerRange rpc, handle case of querying all
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * vendor chaincode before integration tests
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * ignore case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix example
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix test case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix REST API test
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add test case for querying all db entries
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- client/example/client.go
+++ client/example/client.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	fabcli "github.com/hyperledger-labs/fabex/client"
 	"github.com/hyperledger-labs/fabex/helpers"
+	pb "github.com/hyperledger-labs/fabex/proto"
 	"log"
 )
 
@@ -42,9 +43,9 @@ func main() {
 
 	//txs, err := client.GetRange(1, 15)
 	//txs, err := client.Get(&pb.Entry{Txid:"3a3e933a3d9953b0b10e6573254b6d3cf2347d72058c0347a55054babdd8e1a1"})
-	//txs, err := client.Get(&pb.Entry{Payload: "WriteSet"})
-	//txs, err := client.Get(&pb.Entry{Blocknum: 5})
-	txs, err := client.Get(nil)
+	//txs, err := client.Get(&pb.Entry{Payload: "car0"})
+	//txs, err := client.Get(nil)
+	txs, err := client.Get(&pb.Entry{Blocknum: 5})
 	if err != nil {
 		log.Fatal(err)
 	}
