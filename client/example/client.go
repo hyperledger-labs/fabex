03f64dd3935565b856f47b5f27a163bcce11068d
Vadim Inshakov <vadiminshakov@gmail.com>
2020-06-24 15:39:10 +0300

    Compkeys mgmt (#53)
    * Fix CI (#47)
    
    * remove log msg
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * trigger CI on push && PR to 1.x, 2.x branches
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * new example request
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * handle nil and null block values
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add test case for querying all db entries
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * del Explore, add GetRange rpc
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * increment go version
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add handler for GerRange rpc, handle case of querying all
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * vendor chaincode before integration tests
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * refactored API and fixes (#51)
    
    * ignore case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix example
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix test case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix REST API test
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * Fix CI (#47)
    
    * remove log msg
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * trigger CI on push && PR to 1.x, 2.x branches
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * new example request
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * handle nil and null block values
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add test case for querying all db entries
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * del Explore, add GetRange rpc
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * increment go version
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add handler for GerRange rpc, handle case of querying all
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * vendor chaincode before integration tests
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * ignore case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix example
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix test case
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * fix REST API test
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add test case for querying all db entries
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * no need to extract part of the key from the composite key
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add CreateCompositeKey helper func
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add example of constructing composite key for quering txs with composite keys, add more comments
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>
    
    * add example of constructing composite key for quering txs with composite keys, add more comments
    
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- client/example/client.go
+++ client/example/client.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	fabcli "github.com/hyperledger-labs/fabex/client"
 	"github.com/hyperledger-labs/fabex/helpers"
-	pb "github.com/hyperledger-labs/fabex/proto"
 	"log"
 )
 
@@ -41,11 +40,27 @@ func main() {
 	    Use this commented lines for your experiments!
 	 */
 
+	// get txs from blocks with block number range
 	//txs, err := client.GetRange(1, 15)
+
+	// get tx with tx ID
 	//txs, err := client.Get(&pb.Entry{Txid:"3a3e933a3d9953b0b10e6573254b6d3cf2347d72058c0347a55054babdd8e1a1"})
-	//txs, err := client.Get(&pb.Entry{Payload: "car0"})
-	//txs, err := client.Get(nil)
-	txs, err := client.Get(&pb.Entry{Blocknum: 5})
+
+	// get tx with payload key X
+	//txs, err := client.Get(&pb.Entry{Payload: "X"})
+
+	// get txs from specific block
+	//txs, err := client.Get(&pb.Entry{Blocknum: 5})
+
+	// get entry with composite key
+	//key, err := helpers.CreateCompositeKey("RAIL", []string{"1"})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//txs, err := client.Get(&pb.Entry{Payload: key})
+
+    // get all
+	txs, err := client.Get(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
