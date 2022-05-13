9353ec565b1fc39c6380aab6c8ebceb2d8a95a16
Vadim Inshakov <vadiminshakov@gmail.com>
2022-05-13 15:39:10 +0500

    add params for zap logger
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- config/log/log.go
+++ config/log/log.go
@@ -1,12 +1,12 @@
 package log
 
 import (
-	"github.com/hyperledger-labs/fabex/models"
+	"github.com/hyperledger-labs/fabex/config"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
-func GetLogger(config *models.BootConfig) (*zap.Logger, error) {
+func GetLogger(config *config.BootConfig) (*zap.Logger, error) {
 	lvl, err := zap.ParseAtomicLevel(config.LogLevel)
 	if err != nil {
 		return nil, errors.WithStack(errors.Wrap(err, "failed to parse log level"))
@@ -17,6 +17,11 @@ func GetLogger(config *models.BootConfig) (*zap.Logger, error) {
 		DisableCaller:     false,
 		DisableStacktrace: false,
 		Sampling:          nil,
-		Encoding:          "console"}
+		Encoding:          "console",
+		EncoderConfig:     zap.NewProductionEncoderConfig(),
+		OutputPaths:       []string{"stderr"},
+		ErrorOutputPaths:  []string{"stderr"},
+	}
+
 	return l.Build()
 }
